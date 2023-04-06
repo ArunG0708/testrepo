cdc8925297dc1ef0288096283c51f3d98db7a3e8
ImArun <goyalg0708@gmail.com>
2023-04-06 13:05:41 +0530

    update all file

--- js/sero.js
+++ js/sero.js
@@ -56,12 +56,28 @@ function closeNav() {
 
   });
 
-  // for scrolltop
-  function demo() {
-  
-  const element = document.querySelector(body);
-  element.scrollTop > 50;
- 
+
+
+
+
+  const mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+   document.getElementById("myBtn").style.display="block";
+  } else {
+    document.getElementById("myBtn").style.display="none";
+  }
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function demo() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 
 
