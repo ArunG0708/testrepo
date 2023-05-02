6a14f7532cfd31be715e82ce4279af320c764f64
ImArun <goyalg0708@gmail.com>
2023-05-02 12:41:14 +0530

    border radius & aligment update

--- js/sero.js
+++ js/sero.js
@@ -1,6 +1,6 @@
 
 const openNav = () => {
- 
+
   document.getElementById("mySidenav").style.width = "390px";
 }
 
@@ -9,67 +9,65 @@ function closeNav() {
 }
 
 
-  document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
 
-    /////// Prevent closing from click inside dropdown
-    document.querySelectorAll('.dropdown-menu').forEach(function (element) {
-      element.addEventListener('click', function (e) {
-        e.stopPropagation();
-      });
-    })
+  /////// Prevent closing from click inside dropdown
+  document.querySelectorAll('.dropdown-menu').forEach(function (element) {
+    element.addEventListener('click', function (e) {
+      e.stopPropagation();
+    });
+  })
 
 
 
-    // make it as accordion for smaller screens
-    if (window.innerWidth < 1100) {
+  // make it as accordion for smaller screens
+  if (window.innerWidth < 1100) {
 
-      // close all inner dropdowns when parent is closed
-      document.querySelectorAll('.navbar .dropdown').forEach(function (everydropdown) {
-        everydropdown.addEventListener('hidden.bs.dropdown', function () {
-          // after dropdown is hidden, then find all submenus
-          this.querySelectorAll('.submenu').forEach(function (everysubmenu) {
-            // hide every submenu as well
-            everysubmenu.style.display = 'none';
-          });
-        })
-      });
-
-      document.querySelectorAll('.dropdown-menu a').forEach(function (element) {
-        element.addEventListener('click', function (e) {
-
-          let nextEl = this.nextElementSibling;
-          if (nextEl && nextEl.classList.contains('submenu')) {
-            // prevent opening link if link needs to open dropdown
-            e.preventDefault();
-            console.log(nextEl);
-            if (nextEl.style.display == 'block') {
-              nextEl.style.display = 'none';
-            } else {
-              nextEl.style.display = 'block';
-            }
-
-          }
+    // close all inner dropdowns when parent is closed
+    document.querySelectorAll('.navbar .dropdown').forEach(function (everydropdown) {
+      everydropdown.addEventListener('hidden.bs.dropdown', function () {
+        // after dropdown is hidden, then find all submenus
+        this.querySelectorAll('.submenu').forEach(function (everysubmenu) {
+          // hide every submenu as well
+          everysubmenu.style.display = 'none';
         });
       })
-    }
-    // end if innerWidth
+    });
 
-  });
+    document.querySelectorAll('.dropdown-menu a').forEach(function (element) {
+      element.addEventListener('click', function (e) {
+
+        let nextEl = this.nextElementSibling;
+        if (nextEl && nextEl.classList.contains('submenu')) {
+          // prevent opening link if link needs to open dropdown
+          e.preventDefault();
+          console.log(nextEl);
+          if (nextEl.style.display == 'block') {
+            nextEl.style.display = 'none';
+          } else {
+            nextEl.style.display = 'block';
+          }
+
+        }
+      });
+    })
+  }
+  // end if innerWidth
+
+});
 
 
 
-
-
-  const mybutton = document.getElementById("myBtn");
+const mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-   document.getElementById("myBtn").style.display="block";
+    document.getElementById("myBtn").style.display = "block";
   } else {
-    document.getElementById("myBtn").style.display="none";
+    document.getElementById("myBtn").style.display = "none";
   }
 }
 
